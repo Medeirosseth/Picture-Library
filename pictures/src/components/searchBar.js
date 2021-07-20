f7c7a29d760f20368a1ded8b138ff812c326df23
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-20 11:28:29 -0700

    add input state

--- pictures/src/components/searchBar.js
+++ pictures/src/components/searchBar.js
@@ -2,19 +2,24 @@ import React from "react"
 
 
 class searchBar extends React.Component {
+  state = {term: ''};
+
   render() {
     return( 
     <div className="ui segment">
       <form className="ui form">
         <div className="field">
           <label>Image Search</label>
-          <input className="field" type="text"></input>
+          <input 
+            value={this.state.term}
+            onChange={e => this.setState({term: e.target.value})} 
+            className="field" 
+            type="text"></input>
         </div>
       </form>
     </div>
     )
   }
 }
-
 
 export default searchBar;
